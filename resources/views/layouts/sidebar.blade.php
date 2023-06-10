d12ce907ea54d309e3be3f51677871111234c90c
Kevin202510 <72065473+Kevin202510@users.noreply.github.com>
2023-06-10 23:06:57 +0800

    asd

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -5,12 +5,6 @@
         </div>
         <div class="sidebar-brand-text mx-3">SIS</div>
     </a>
-    <hr class="sidebar-divider my-0">
-    <li class="nav-item">
-        <a class="nav-link" href="index.html">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span></a>
-    </li>
     <hr class="sidebar-divider">
     <div class="sidebar-heading">
         Users Management
